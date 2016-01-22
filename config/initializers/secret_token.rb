c2a95d311caee37952afe471f42c7ddb372071a7
Kamal Garg <rajeshwarijadeja36@gmail.com>
2016-01-22 12:06:11 +0530

    initialization app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogStar::Application.config.secret_token = '27aadfdd357ecd67d90737a858e4a3ab70fda6430ae6b85aa225d357d654667c646dc82a5cd4ff73de659669f48aec1c881094ab3f36be4480f39154e35e4abc'
